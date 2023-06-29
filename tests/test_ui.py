14297bacfaf2004e95091f981ede2c6a7cf00b58
spinusimona@yahoo.com <spinusimona@yahoo.com>
2023-06-29 17:55:14 +0300

    without headless

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
 
 option = webdriver.FirefoxOptions()
-option.add_argument("--headless")
+#option.add_argument("--headless")
 option.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
 driverService = Service('C:\\5SIMU\\python-test-calculator\\WebDriver\\geckodriver.exe')
 driver = webdriver.Firefox(service=driverService, options=option)
