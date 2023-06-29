226ebc2e45933db972eb280d2d954abd92c9885f
spinusimona@yahoo.com <spinusimona@yahoo.com>
2023-06-29 14:14:08 +0300

    updated test

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -2,12 +2,30 @@
 from src.calculator import  add
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-driver = webdriver.Firefox()
-#from webdriver_manager.chrome import ChromeDriverManager
-#driver = webdriver.Chrome(ChromeDriverManager().install())
+from selenium.webdriver.chrome.service import Service
+from chromedriver_py import binary_path
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import pytest
 import time
 
+@pytest.fixture()
+def test_setup():
+    service_object = Service(binary_path)
+    global driver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--window-size=1920x1080")
+    driver = webdriver.Chrome(service=service_object)
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield
+    driver.quit()
 
 def test_add():
     result = add(3, 4)
@@ -15,9 +33,11 @@ def test_add():
 def test_add_string():
     with pytest.raises(TypeError):
         add("string", 4)
-def test_SMDB():
-    driver.get('https://test-outsystems.de.bosch.com/SensorTrackingMetaData_Solution/')
-    time.sleep(10)
-    driver.find_element(By.XPATH,'//*[@role="menuitem" and text()="Treatments"]').click()
-    time.sleep(5)
-    driver.quit()
+def test_SMDB(test_setup):
+    #driver.find_element(By.XPATH,'//*[@role="menuitem" and text()="Treatments"]').click()
+
+    driver.get("https://docs.pytest.org/en/7.3.x/")
+    searchField = driver.find_element(By.NAME, "q")
+    searchField.send_keys("test")
+    searchField.send_keys(Keys.ENTER)
+    assert "test" in driver.current_url
